daa313fab75ce2950fc690f2c7c20b50fa84d608
TatianaShulgina2022 <t.shulgina@directline.digital>
2023-03-24 23:37:09 +0400

    сделал первую домашку, начал вторую
    время 3:56, сижу с девяти вечера, супер интересно

---
+++ homeworks/1 lection/script.js
@@ -0,0 +1,17 @@
+let num = 30.25;
+let crypto = 5165414775176541n;
+let str = "chubarek";
+let deadInside = true;
+let brain = null;
+let x;
+let id = Symbol("id")
+let user = { name: "Серёжа"}
+
+console.log(Number(num), String(num), Boolean(num));
+console.log(Number(crypto), String(crypto), Boolean(crypto));
+console.log(Number(str), String(str), Boolean(str));
+console.log(Number(deadInside), String(deadInside), Boolean(deadInside));
+console.log(Number(brain), String(brain), Boolean(brain));
+console.log(Number(x), String(x), Boolean(x));
+console.log(Number(id), String(id), Boolean(id));
+console.log(Number(user), String(user), Boolean(user));
